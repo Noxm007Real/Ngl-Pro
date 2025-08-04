1ffcd075d005d33e7b22fb2ddc12fbc2220c5efe
Noxm007Real <nelson.110103@gmail.com>
2025-08-04 20:16:50 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    // Pastikan server dev mengenali folder api
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
 });
